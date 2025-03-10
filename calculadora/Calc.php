ddbaa6507fcb3c09296d6a7a227950be774b127c
Feffepjl <feliperodriguesmartins15@hotmail.com>
2025-03-10 17:12:43 -0300

    reformulação exercício calculadora

--- calculadora/Calc.php
+++ calculadora/Calc.php
@@ -1,15 +1,3 @@
-<?php
-    include 'calculadora/Calculadora.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $valor1 = $_POST['valor1'];
-    $valor2 = $_POST['valor2'];
-    $operacao = $_POST['operacao'];
-
-    $calculadora = new Calculadora($valor1, $valor2, $operacao);
-    echo $calculadora->Calcular();
-
-}
-?>
 
 
 <!DOCTYPE html>
@@ -20,19 +8,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 <h2>Calculadora</h2>
-<form action="calculadora/calc.php" method="POST">
+<form action="" method="POST">
     <input type="text" name="valor1" placeholder="Digite o primeiro número" required>
     <input type="text" name="valor2" placeholder="Digite o segundo número" required>
     <select name="operacao">
-        <option value="+">+</option>
+        <option value="+" name="+" id="+">+</option>
         <option value="-">-</option>
         <option value="*">X</option>
         <option value="/">/</option>
     </select>
-    <button type="submit">=</button>
+    <button type="submit" name="btnVerificar" id="btnVeriicar">=</button>
     <br><br>
+<?php
+    include 'Calculadora.php';
+    if (isset($_POST['btnVerificar'])) {
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $operacao = $_POST['operacao'];
+
+        $calculadora = new Calculadora($valor1, $valor2, $operacao);
+        $resultado = $calculadora->Calcular();
+
+    }
+?>
     <label>Resultado:</label>
-    <input type="text" value="<?= $resultado ?>" readonly>
+    <input type="text" value="<?php echo $resultado ?>" readonly>
 </form>
 </body>
 </html>
